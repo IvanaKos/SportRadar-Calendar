629f3a9af1dacd04a02f7b7bd30a07bf1e616721
Ivana <kostevski.ivana@gmail.com>
2023-11-19 11:10:39 +0100

    Link pages

---
+++ pages/add-event.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    <NuxtLink to="/">Home</NuxtLink>
+    <h1>Add Event</h1>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
+    <NuxtLink to="/add-event">Add Event</NuxtLink>
     <h1>Hello</h1>
   </div>
 </template>
 <script></script>
+<style>
+h1 {
+  color: red;
+}
+</style>
