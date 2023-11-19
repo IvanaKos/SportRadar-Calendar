ea0364cc7c753388688e5faa6ec3b3430e2d700f
Ivana <kostevski.ivana@gmail.com>
2023-11-19 17:28:56 +0100

    Integrate vcalendar

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,24 @@
   <div>
     <NuxtLink to="/add-event">Add Event</NuxtLink>
     <h1>Hello</h1>
+    <VCalendar v-model="date" />
+    <VDatePicker v-model="date" :attributes="attrs" />
   </div>
 </template>
-<script></script>
+<script setup lang="ts">
+import { ref } from "#imports";
+const date = ref(new Date());
+const attrs = ref([
+  {
+    key: "today",
+    highlight: {
+      color: "green",
+      fillMode: "solid",
+    },
+    dates: new Date(),
+  },
+]);
+</script>
 <style>
 h1 {
   color: red;
