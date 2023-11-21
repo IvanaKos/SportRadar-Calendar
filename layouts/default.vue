dd79ed648d4dceab568f894b59aa8e3eef4b0f73
Ivana <kostevski.ivana@gmail.com>
2023-11-21 09:07:47 +0100

    Add layout and calendar initial style

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div><NuxtPage /></div>
+</template>
+<script></script>
+<style>
+.page {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,33 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
+  <div class="page">
     <NuxtLink to="/add-event">Add Event</NuxtLink>
     <h1>Hello</h1>
     <VCalendar v-model="date" />
-    <VDatePicker v-model="date" :attributes="attrs" />
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from "#imports";
 const date = ref(new Date());
-const attrs = ref([
-  {
-    key: "today",
-    highlight: {
-      color: "green",
-      fillMode: "solid",
-    },
-    dates: new Date(),
-  },
-]);
 </script>
 <style>
+.vc-header {
+  padding: 50px;
+}
+.vc-title {
+  background: none;
+}
+.vc-title,
+.vc-weekday,
+.vc-day-content {
+  font-size: 30px;
+  padding: 40px;
+}
 h1 {
   color: red;
+}
+.vc-container {
+  --day-content-height: 150px;
+  --day-content-width: 150px;
 }
 </style>
