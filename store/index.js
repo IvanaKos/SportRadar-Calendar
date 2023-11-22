1aec0ee9100b1d119ce3d3b6efeb423447fa12d7
Ivana <kostevski.ivana@gmail.com>
2023-11-22 17:01:03 +0100

    Add event status

--- store/index.js
+++ store/index.js
@@ -3,7 +3,7 @@ import eventListData from "/src/event-list.json";
 const eventsList = eventListData.data;
 const calendarData = eventsList.map((event) => ({
   key: event.dateVenue + " " + event.timeVenueUTC,
-  dot: true,
+  dot: event.status === "played" ? "red" : "blue",
   dates: event.dateVenue,
 }));
 
