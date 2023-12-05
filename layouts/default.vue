b0f0a0586e03cd5dc1acd707614f003185f66e98
Ivana <kostevski.ivana@gmail.com>
2023-12-05 13:31:29 +0100

    Add media queries for calendar

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,4 +30,37 @@ export default {
   font-size: 1.4rem;
   padding: 25px;
 }
+
+@media only screen and (max-width: 768px) {
+  .vc-header {
+    padding: 15px;
+  }
+
+  .vc-title {
+    background: none;
+  }
+
+  .vc-title,
+  .vc-weekday,
+  .vc-day-content {
+    font-size: 1.1rem;
+    padding: 20px;
+  }
+  @media only screen and (max-width: 576px) {
+    .vc-header {
+      padding: 8px;
+    }
+
+    .vc-title {
+      background: none;
+    }
+
+    .vc-title,
+    .vc-weekday,
+    .vc-day-content {
+      font-size: 1.1rem;
+      padding: 12px;
+    }
+  }
+}
 </style>
